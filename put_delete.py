f99aa2ac7ee8ca44ca6567d6a084d72eca1cd928
Jayanta nath <nathjayanta772@gmail.com>
2025-11-05 19:36:21 +0530

    updated

--- put_delete.py
+++ put_delete.py
@@ -85,3 +85,17 @@ def update_patient(patient_id: str, pat_update: PatientUpdate):
 
     return JSONResponse(status_code=200, content={"message": "Patient Updated"})
 
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code = 404, detail = "Patient not Exists")
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'Patient deleted'})
